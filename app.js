983e8f8c9ac581163a2952f8a9fa2fd46bfe89ad
Carla Borges Reis <carlareis.dev.com>
2023-10-27 16:33:29 -0700

    error handlers fixed

--- app.js
+++ app.js
@@ -17,15 +17,16 @@ app.get("/about", (req, res) => {
     res.render("about");
 });
 
-app.get("/projects/:id", (req, res) => {
-    res.render('project', {
-        project_name: projects[req.params.id].project_name,
-        description: projects[req.params.id].description,
-        technologies: projects[req.params.id].technologies,
-        live_link: projects[req.params.id].live_link,
-        github_link: projects[req.params.id].github_link,
-        image_urls: projects[req.params.id].image_urls
-    });
+app.get("/projects/:id", (req, res, next) => {
+    const project = projects.find(({ id }) => id === +req.params.id);
+    if (project) {
+        res.render('project', { project });
+    } else {
+        const err = new Error();
+        err.status = 404;
+        err.message = `It looks like the project requested doesn't exist.`;
+        next(err);
+    }
 });
 
 /* ERROR HANDLERS */
@@ -37,9 +38,9 @@ app.use((req, res, next) => {
 /* Global error handler */
 app.use((err, req, res, next) => {
     if (err.status === 404) {
-        console.log(err.status, err.message);
-        res.status(404).render("not-found", { err });
+        res.render("not-found", { err });
     } else {
+        const err = new Error();
         err.message = err.message || `Oops! Something went wrong on the server!`
         res.status(err.status || 500).render('error', { err });
     }
